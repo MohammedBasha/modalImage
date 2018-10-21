c9514bd57fbb3fd610ca04088f6035ae27b4aaf4
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-21 22:40:29 +0200

    Resizing the images width and height

--- js/scripts.js
+++ js/scripts.js
@@ -21,13 +21,35 @@
       overlayDiv.style.backgroundColor = "rgba(0, 0, 0, .7)";
       overlayDiv.style.cursor = "pointer";
 
+      // Creating and adjust the images styles
       var imageSrc = e.target.src,
           largeImage = document.createElement("img");
       largeImage.id = "large-img";
       largeImage.src = imageSrc.substr(0, imageSrc.length - 7) + ".jpg";
       largeImage.style.position = "absolute";
       largeImage.style.display = "block";
-      overlayDiv.appendChild(largeImage);
+
+      // after image loads do the size styles
+      largeImage.addEventListener("load", function () {
+
+        // Resizing the image height if it was taller than the window height
+        if (this.height > window.innerHeight) {
+          this.ratio = window.innerHeight / this.height;
+          this.height *= this.ratio;
+          this.width *= this.ratio;
+        }
+
+        // Resizing the image width if it was wider than the window width
+        if (this.width > window.innerWidth) {
+          this.ratio = window.innerWidth / this.width;
+          this.height *= this.ratio;
+          this.width *= this.ratio;
+        }
+
+        overlayDiv.appendChild(largeImage); // append it after changes
+
+      });
+
 
     } // targeting the images only
   }, false); // fires when any image is clicked
