627d5da7ee835f7b1fcf7a0da90452e6ce1acb10
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-21 22:29:09 +0200

    adding the imagas a large ine over the overlay

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,30 @@
   
   myUl.addEventListener("click", function (e) {
     if (e.target.tagName === "IMG") {
-      console.log(e);
+
+      var overlayDiv = document.createElement("div");
+      overlayDiv.id = "overlay";
+      document.body.appendChild(overlayDiv); // appending an overlay div to the end of the body
+
+      // Styling the overlay div
+      overlayDiv.style.position = "absolute";
+      overlayDiv.style.top = window.pageYOffset + "px";
+      overlayDiv.style.left = window.pageXOffset + "px";
+
+      overlayDiv.style.width = window.innerWidth + "px";
+      overlayDiv.style.height = window.innerHeight + "px";
+      
+      overlayDiv.style.backgroundColor = "rgba(0, 0, 0, .7)";
+      overlayDiv.style.cursor = "pointer";
+
+      var imageSrc = e.target.src,
+          largeImage = document.createElement("img");
+      largeImage.id = "large-img";
+      largeImage.src = imageSrc.substr(0, imageSrc.length - 7) + ".jpg";
+      largeImage.style.position = "absolute";
+      largeImage.style.display = "block";
+      overlayDiv.appendChild(largeImage);
+
     } // targeting the images only
   }, false); // fires when any image is clicked
 
